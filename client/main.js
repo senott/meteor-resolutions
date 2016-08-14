05a5c2083dd3c17d2a58029cd4ebc655edef81a8
Senott <senott@yahoo.com>
2016-08-13 23:58:26 -0300

    @DASMacBookAir: video 14 - making app more secure.

--- client/main.js
+++ client/main.js
@@ -25,10 +25,7 @@ Template.body.events({
   'submit .new-resolution': function(event){
     var title = event.target.title.value;
 
-    Resolutions.insert({
-      title: title,
-      createdAt: new Date()
-    });
+    Meteor.call("addResolution", title);
 
     event.target.title.value = "";
 
--- server/main.js
+++ server/main.js
@@ -3,3 +3,12 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+Meteor.methods({
+  addResolution: function(title){
+    Resolutions.insert({
+      title: title,
+      createdAt: new Date()
+    });
+  }
+});
